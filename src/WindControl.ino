7f15e2a04959527d919aa558db89613eb82eed0a
Blaise Cador <blaise.cador@gmail.com>
2017-04-19 23:47:04 +0200

    ajout du compte nombre de tour

--- src/WindControl.ino
+++ src/WindControl.ino
@@ -2,12 +2,13 @@
 #include "ChainableLED.h"
 
 #define NUM_LEDS  1
-#define TIME 10
+#define TIME 50
 // Init led
 ChainableLED leds(2,3, NUM_LEDS);
 
 
-int i,j,t,count;
+int i,j,count,turns;
+int t;
 
 void setup() {
   Serial.begin(9600);
@@ -21,12 +22,13 @@ void loop() {
   leds.setColorRGB(0, 0, 15, 0);
   i = 0;
   j = 0;
+  turns = 0;
   t = 0;
   //int sensorValue = analogRead(A1);
   //Serial.println(sensorValue);
 
 // s'arrete lorsque la roue a fait 1 tour complet
-  while(i + j != 2){
+  while(turns < 10){
     int BP = digitalRead(4);        // Lecture du capteur
 
     if (BP == LOW) {
@@ -36,14 +38,31 @@ void loop() {
       digitalWrite(13, LOW);      // Eteind la Led
       j = 1;
     }
-    Serial.println(i + j);
+
+
+    //Serial.print(i);
+    //Serial.print(" ");
+    //Serial.println(j);
+
+
+    // Compte le nombre de tour
+    if(i == 1 && j == 1){
+      turns += 1;
+      Serial.print("Nombre de tour = ");
+      Serial.println(turns);
+      i = 0;
+      j = 0;
+    }
+
     t = t + TIME; // temps passé pour faire un tour complet
     delay(TIME);
+
+    Serial.println(t);
   }
 
 
 // Si temps court : ya du vent
-  if(t < 200){
+  if(t < 2000){
     leds.setColorRGB(0, 15, 15, 0);
     Serial.print("t = ");
     Serial.println(t);
