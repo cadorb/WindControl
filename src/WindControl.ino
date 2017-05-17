aec0af372d36befd7717f60bbd2e3f1739762ffa
Blaise Cador <blaise.cador@gmail.com>
2017-05-17 15:08:09 +0200

    tables de correspondances

--- src/WindControl.ino
+++ src/WindControl.ino
@@ -1,84 +1,11 @@
-// LED
-#include "ChainableLED.h"
-
-#define NUM_LEDS  1
-#define TIME 50
-// Init led
-ChainableLED leds(2,3, NUM_LEDS);
-
-
-int i,j,count,turns;
-long t;
+#include "Arduino.h"
 
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(4, INPUT);
-  leds.init();
-  leds.setColorRGB(0, 0, 15, 0);
-  count = 0;
+  delay(2000);
+  Serial.println("Starting Giro control");
 }
 void loop() {
-  leds.setColorRGB(0, 0, 15, 0);
-  i = 0;
-  j = 0;
-  turns = 0;
-  t = 0;
-
-
-// s'arrete lorsque la roue a fait 1 tour complet
-  while(turns < 10){
-    int BP = digitalRead(4);        // Lecture du capteur
-
-    if (BP == LOW) {
-      digitalWrite(13, HIGH);      // Allume la Led
-      i = 1;
-    }else {
-      digitalWrite(13, LOW);      // Eteind la Led
-      j = 1;
-    }
-
-
-    //Serial.print(i);
-    //Serial.print(" ");
-    //Serial.println(j);
-
-
-    // Compte le nombre de tour
-    if(i == 1 && j == 1){
-      turns += 1;
-      Serial.print("Nombre de tour = ");
-      Serial.println(turns);
-      i = 0;
-      j = 0;
-    }
-
-    t = t + TIME; // temps passé pour faire 10 tours complets
-    delay(TIME);
-
-    Serial.println(t);
-  }
-
-
-// Si temps court : ya du vent
-  if(t < 2000){
-    leds.setColorRGB(0, 15, 15, 0);
-    Serial.print("t = ");
-    Serial.println(t);
-    Serial.println("Putain ya du vent !");
-    count = count + 1;
-    delay(500);
-    // Si le vent dure un certain temps : on stop tout
-    if(count >= 2){
-      leds.setColorRGB(0, 255, 0, 0);
-      int sensorValue = analogRead(A1);
-      Serial.print("Mise en securite de la parabole en :");
-      Serial.println(sensorValue);
-      count = 0;
-      delay(5000);
-    }
-  }
-
-
-
+      Serial.println(windDirection());
+      delay(1000);
 }
---
+++ src/functions.ino
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+float windDirection(){
+  int gyroValues[16] = {889,707,790,788,409,464,82,92,65,185,126,290,247,633,946,831}; // Resistances of the gyro
+  float angleValues[17] = {0,22.5,45,67.5,90,112.5,135,157.5,180,202.5,225,247.5,270,292.5,315,337.5,360}; // Angles corresponding to resistances
+  int indice = 17;
+  float windCardinal;
+
+  int windValue = analogRead(A1);
+  Serial.print("Value  = ");
+  Serial.println(windValue);
+
+
+  for (int i = 0; i < 16; ++i) {
+    if (gyroValues[i]==windValue) {
+      indice = i;
+      Serial.print("i  = ");
+      Serial.println(i);
+  }
+}
+
+  Serial.print("indice  = ");
+  Serial.println(indice);
+
+  windCardinal = angleValues[indice];
+
+  Serial.print("windCardinal  = ");
+  Serial.println(windCardinal);
+
+  return windCardinal;
+}
